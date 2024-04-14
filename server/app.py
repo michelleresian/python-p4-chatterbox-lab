57fb9ab3ce615c36251558ca8e30ed65da584694
michelleresian <michellresian@gmail.com>
2024-04-14 12:11:45 +0300

    chatterbox

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at).all()
+    messages_serialized = [msg.to_dict() for msg in messages]
+    return make_response(jsonify(messages_serialized), 200)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return make_response(jsonify(new_message.to_dict()), 201)
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        return make_response(jsonify({'message': 'Message not found'}), 404)
+
+    data = request.get_json()
+    message.body = data.get('body', message.body)
+    db.session.commit()
+    return make_response(jsonify(message.to_dict()), 200)
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id)
+
+    if not message:
+        return make_response(jsonify({'message': 'Message not found'}), 404)
+
+    db.session.delete(message)
+    db.session.commit()
+    return make_response(jsonify({'message': 'Message deleted successfully'}), 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
